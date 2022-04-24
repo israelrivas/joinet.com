bc64a5e4dfe49afee767ef447c35eac8f7d56528
pikino84 <jcruz@gruporosanegra.com.mx>
2022-04-24 18:37:52 -0500

    actualizacion de archivos

--- Views/Template/header_tienda.php
+++ Views/Template/header_tienda.php
@@ -135,17 +135,15 @@
 			</div>
 
 			<div class="wrap-menu-desktop">
-			
 				<nav class="limiter-menu-desktop container">
-				
 					
 					<!-- Logo desktop -->		
 					<a href="<?= base_url(); ?>" class="logo">
-						<img src="<?= media() ?>/tienda/images/logo.png" alt="Joinet.com | Tu tienda en linea">
+						<img src="<?= media() ?>/tienda/images/logo.png" alt="Tienda Virtual">
 					</a>
 
 					<!-- Menu desktop -->
-					<!-- <div class="menu-desktop">
+					<div class="menu-desktop">
 						<ul class="main-menu">
 							<li class="active-menu">
 								<a href="<?= base_url(); ?>">Inicio</a>
@@ -171,15 +169,15 @@
 								<a href="<?= base_url(); ?>/contacto">Contacto</a>
 							</li>
 						</ul>
-					</div>	 -->
+					</div>	
 
 					<!-- Icon header -->
 					<div class="wrap-icon-header flex-w flex-r-m">
-						<!-- <div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 js-show-modal-search">
-							<i class="zmdi zmdi-search"></i> -->
+						<div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 js-show-modal-search">
+							<i class="zmdi zmdi-search"></i>
 						</div>
 						<?php if($data['page_name'] != "carrito" and $data['page_name'] != "procesarpago"){ ?>
-						<div class="cantCarrito icon-header-item cl0 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti js-show-cart" data-notify="<?= $cantCarrito; ?> ">
+						<div class="cantCarrito icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti js-show-cart" data-notify="<?= $cantCarrito; ?> ">
 							<i class="zmdi zmdi-shopping-cart"></i>
 						</div>
 						<?php } ?>
@@ -188,48 +186,11 @@
 			</div>	
 		</div>
 
-
-		<div class="container-menu-desktop">
-			<!-- Topbar -->
-			<div class="button-bar">
-				<div class="content-topbar flex-sb-m h-full container">
-					<div class="left-top-bar">
-						<?php if(isset($_SESSION['login'])){ ?>
-						Bienvenido: <?= $_SESSION['userData']['nombres'].' '.$_SESSION['userData']['apellidos'] ?>
-						<?php } ?>
-					</div>
-
-					<div class="right-top-bar flex-w h-full">
-						<a href="#" class="flex-c-m trans-04 p-lr-25" data-toggle="modal" data-target="#modalAyuda" >
-							Help & FAQs
-						</a>
-						<?php 
-							if(isset($_SESSION['login'])){
-						?>
-						<a href="<?= base_url() ?>/dashboard" class="flex-c-m trans-04 p-lr-25">
-							Mi cuenta
-						</a>
-						<?php } 
-							if(isset($_SESSION['login'])){
-						?>
-						<a href="<?= base_url() ?>/logout" class="flex-c-m trans-04 p-lr-25">
-							Salir
-						</a>
-						<?php }else{ ?>
-						<a href="<?= base_url() ?>/login" class="flex-c-m trans-04 p-lr-25">
-							Iniciar Sesión
-						</a>
-						<?php } ?>
-					</div>
-				</div>
-			</div>
-
-
 		<!-- Header Mobile -->
 		<div class="wrap-header-mobile">
 			<!-- Logo moblie -->		
 			<div class="logo-mobile">
-				<a href="<?= base_url(); ?>"><img src="<?= media() ?>/tienda/images/logo.png" alt="Joinet.com | Tu tienda en linea"></a>
+				<a href="<?= base_url(); ?>"><img src="<?= media() ?>/tienda/images/logo.png" alt="Tienda Virtual"></a>
 			</div>
 
 			<!-- Icon header -->
---
+++ metodos.php
@@ -0,0 +1,19 @@
+<?php
+//8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92
+
+$encript =  openssl_decrypt('jbcx+0ijz5HTHdtQ19lC3w==','AES-128-ECB','abelosh');
+echo $encript."<br>";
+
+
+$decript =  openssl_encrypt('123456','AES-128-ECB','abelosh');
+echo $decript;
+
+/*
+jeni martinez 
+ckaditivo
+
+$2542
+
+carta de no adeudo.
+
+*/
